f5bf067328657c9343fcff98b3c065a1338ee668
prosopher <prosopher@gmail.com>
2026-04-12 13:35:52 +0900

    Fix test

--- lsc/train.py
+++ lsc/train.py
@@ -15,7 +15,7 @@ from core.train_util import *
 @dataclass
 class TrainConfig(Config):
     model_ids: str
-    model_directions: str = "all"
+    model_directions: str
     max_steps: int
     batch_size: int
     grad_accum_steps: int
