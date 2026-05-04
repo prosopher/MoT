2845bcc849cdc9d37267336ec569c28364d73a02
prosopher <prosopher@gmail.com>
2026-05-04 12:05:03 +0900

    Set return KV to (attention_key, attention_value)

--- mot/train.py
+++ mot/train.py
@@ -900,7 +900,7 @@ def run_gpt2_block(
     attn_output = attn.resid_dropout(attn_output)
     hidden_states = residual + attn_output
     hidden_states = hidden_states + block.mlp(block.ln_2(hidden_states))
-    return hidden_states, (native_like_key, native_like_value)
+    return hidden_states, (attention_key, attention_value)
 
 
 def run_opt_block(
@@ -987,7 +987,7 @@ def run_opt_block(
     hidden_states = (residual + hidden_states).view(hidden_states_shape)
     if not getattr(block, "do_layer_norm_before", False):
         hidden_states = block.final_layer_norm(hidden_states)
-    return hidden_states, (native_like_key, native_like_value)
+    return hidden_states, (attention_key, attention_value)
 
 
 def rotate_half(x: torch.Tensor) -> torch.Tensor:
