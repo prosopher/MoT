599489e4e2285c24bef2280ebcc269e8897fcf21
prosopher <prosopher@gmail.com>
2026-05-08 16:22:54 +0900

    Update loss to match main

--- exp/layer_position.py
+++ exp/layer_position.py
@@ -444,11 +444,13 @@ def run_train(
                     tgt_spec=ctx.mm.get_model_spec(edge.tgt_id),
                     target_model_id=node_map[edge.tgt_id].model_id,
                 )
-                total_direction_loss = total_direction_loss + compute_suffix_lm_loss(
+                total_direction_loss = total_direction_loss + compute_prefix_correction_and_suffix_lm_loss(
                     target_model=ctx.mm.get_model(edge.tgt_id),
                     past_key_values=mixed_target_past,
                     lm_input_ids=lm_input_ids,
                     lm_labels=lm_labels,
+                    native_target_past_key_values=past_by_node_id[edge.tgt_id],
+                    target_layer_indices=ctx.cm.get_tgt_layer_indices(edge.id),
                 )
 
             loss = total_direction_loss / config.grad_accum_steps
